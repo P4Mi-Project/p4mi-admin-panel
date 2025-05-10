93670672c6be6b719e290e33e428309c865ff79e
Md Masud karim <msmasud578@gmail.com>
2025-05-11 00:00:41 +0200

    added the dashboard route and also the route page component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import  "bootstrap/dist/css/bootstrap.min.css"
+
+const Dashboard = ()=> {
+    return (
+    <div className = "bg-dark container-fluid" style = {{height: "100vh"}} >
+        <h1>hello world</h1>
+    </div>
+    );
+}
+
+export default Dashboard;
